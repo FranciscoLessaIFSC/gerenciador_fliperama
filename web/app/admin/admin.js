df2249b249dce8cd5f9ce76083e8bcdd8024e410
Arthur Moro <arthur.moro2408@gmail.com>
2026-06-19 16:19:37 -0300

    commit graficos dashboard

--- web/app/admin/admin.js
+++ web/app/admin/admin.js
@@ -1,3 +1,4 @@
+
 const sidebarToggleButtons = document.querySelectorAll("[data-sidebar-toggle]");
 const root = document.body;
 const sidebarStateKey = "arcade-admin-sidebar-collapsed";
@@ -39,3 +40,167 @@ window.addEventListener("resize", () => {
 		readInitialState();
 	}
 });
+
+/* ==========================
+   GRÁFICO DE JOGOS DIÁRIOS
+========================== */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+	const canvas = document.getElementById("dailyGamesChart");
+
+	if (!canvas) return;
+
+	new Chart(canvas, {
+		type: "line",
+
+		data: {
+			labels: ["Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"],
+
+			datasets: [{
+				data: [1180, 1240, 1320, 1275, 1426, 1680, 1540],
+
+				borderColor: "#7aa2ff",
+				backgroundColor: "rgba(122,162,255,0.15)",
+
+				fill: true,
+				tension: 0.4,
+
+				borderWidth: 3,
+
+				pointRadius: 0
+			}]
+		},
+
+		options: {
+			responsive: true,
+			maintainAspectRatio: false,
+
+			plugins: {
+				legend: {
+					display: false
+				},
+				tooltip: {
+					enabled: true
+				}
+			},
+
+			scales: {
+				x: {
+					display: false
+				},
+				y: {
+					display: false
+				}
+			}
+		}
+	});
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+
+	const canvas = document.getElementById("OnCardsChart");
+
+	if (!canvas) return;
+
+	new Chart(canvas, {
+		type: "line",
+
+		data: {
+			labels: ["Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"],
+
+			datasets: [{
+				data: [760, 750,700, 790, 842, 900, 893],
+
+				borderColor: "#7aa2ff",
+				backgroundColor: "rgba(122,162,255,0.15)",
+
+				fill: true,
+				tension: 0.4,
+
+				borderWidth: 3,
+
+				pointRadius: 0
+			}]
+		},
+
+		options: {
+			responsive: true,
+			maintainAspectRatio: false,
+
+			plugins: {
+				legend: {
+					display: false
+				},
+				tooltip: {
+					enabled: true
+				}
+			},
+
+			scales: {
+				x: {
+					display: false
+				},
+				y: {
+					display: false
+				}
+			}
+		}
+	});
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+
+	const canvas = document.getElementById("DailyRevenueChart");
+
+	if (!canvas) return;
+
+	new Chart(canvas, {
+		type: "line",
+
+		data: {
+			labels: ["Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"],
+
+			datasets: [{
+				data: [790, 890, 999, 980, 1950, 2150, 2000],
+
+				borderColor: "#7aa2ff",
+				backgroundColor: "rgba(122,162,255,0.15)",
+
+				fill: true,
+				tension: 0.4,
+
+				borderWidth: 3,
+
+				pointRadius: 0
+			}]
+		},
+
+		options: {
+			responsive: true,
+			maintainAspectRatio: false,
+
+			plugins: {
+				legend: {
+					display: false
+				},
+				tooltip: {
+					enabled: true
+				}
+			},
+
+			scales: {
+				x: {
+					display: false
+				},
+				y: {
+					display: false
+				}
+			}
+		}
+	});
+});
+
+
+
+
